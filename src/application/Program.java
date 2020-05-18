111b628aef26602ef60987b7267ba32ab93022ac
ribeirojunior <sergio.rivusjr@gmail.com>
2020-05-18 17:57:42 -0300

    Programa Construido

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,45 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter Account Data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			System.out.print("Initial Balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw Limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			Account account = new Account(number, holder, balance, withdrawLimit);
+			
+			System.out.print("Enter the amount for deposit: ");
+			double deposit = sc.nextDouble();
+			System.out.print("Enter the amount for withdraw: ");
+			double withdraw = sc.nextDouble();
+			
+			account.uptadeBalance(deposit, withdraw);
+			
+			System.out.println("New Balance: " + account.getBalance());
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		
+		sc.close();
 	}
 
 }
